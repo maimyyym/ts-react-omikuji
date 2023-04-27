88cff1b44694f879cf7166a5b3f4810b84c1ab3f
maimy <m.711029@gmail.com>
2023-04-27 23:47:03 +0900

    不要箇所の削除、コメントアウト追記

--- src/hooks/useDrawOmikuji.ts
+++ src/hooks/useDrawOmikuji.ts
@@ -2,24 +2,23 @@ import { useState } from "react";
 
 const omikujiList = ['大吉', '中吉', '吉', '小吉', '末吉', '凶'] as const;
 
-
+// おみくじ結果を配列から抽出した個々の型を定義＋export
 export type OmikujiResult = typeof omikujiList[number];
 
 
-
 function useDrawOmikuji() {
-    // drawing中であれば、setTimeoutで一定の回数だけ一定間隔でおみくじ表示関数を呼び出す。
-    // おみくじ表示関数
-    // drawOmikujiで回数を指定。
 
+  // おみくじ結果とおみくじを引いている状態の管理
   const [result, setResult] = useState<OmikujiResult | ''>('');
   const [isDrawing, setIsDrawing] = useState(false);
 
+  // おみくじを引く関数を定義
   const drawOmikuji = () => {
 
     const maxCount = 10;
     setIsDrawing(true);
 
+    // おみくじ結果を取得、setTimeoutで200msごとに再帰的にmaxCountで定義した回数だけ繰り返す。（200ms × maxCountの間ランダムで表示される）
     const omikujiResult = (count: number) => {
 
         if (count >= maxCount) {
